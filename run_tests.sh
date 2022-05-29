0e42174924dc67ce3913950ced73946e5cfbd2c4
Denis Afanasev <Denis.Afanasev@gmail.com>
2022-05-29 22:56:17 +0300

    fix test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,10 @@
 # conda activate nhc_dev
 conda env export --no-builds > environment.yml
 
-zip -r data_backup.zip data
+# zip -r data_backup.zip data
 
 python -m coverage run tests/TestNeuroHealthApp.py
 python -m coverage report
 
-rm -r data
-unzip data_backup.zip
+# rm -r data
+# unzip data_backup.zip
